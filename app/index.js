98073cc1da008ec4de3e89def53678072ddfc0ee
shivprogrammer <shiv.unadkat@gmail.com>
2019-05-10 13:00:34 -0700

    JSX cleanup for more concise code on index.js

--- app/index.js
+++ app/index.js
@@ -13,17 +13,12 @@ function isNew() {
 class App extends React.Component {
   render() {
     const authed = isAuthed()
-    const newUser = isNew()
 
-    if (newUser === true) {
-      return <h1>Welcome for your first time!</h1>
-    }
-    else if (authed === true) {
-      return <h1>Welcome back!</h1>
-    }
-    else {
-      return <h1>Login to see your dashboard</h1>
-    }
+    return (
+      <div>
+        {authed === true && <h3>You are authed!</h3>}
+      </div>
+    )
   }
 }
 
