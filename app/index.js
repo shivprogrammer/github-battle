15a9a868594e5df57260ed1c453ace20452c3f67
shivprogrammer <shiv.unadkat@gmail.com>
2019-05-10 12:47:09 -0700

    working on some JSX functionality

--- app/index.js
+++ app/index.js
@@ -2,11 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+function isLoading() {
+  return True
+}
+
 class App extends React.Component {
   render() {
+    if (isLoading() === true) {
+      return null
+    }
+
+    const name = 'Shiv'
     return (
       <div>
-        Hello World!
+        <h1>Hello, {name}</h1>
+        <p>Today is {new Date().toLocaleString()}</p>
+        <p>What is 2 + 2? {2 + 2}</p>
       </div>
     )
   }
