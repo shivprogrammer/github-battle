736e2781525bc9d28c4e7c21cd71c92b91b6175d
shivprogrammer <shiv.unadkat@gmail.com>
2019-05-11 02:38:02 -0700

    still a bit more playing with the 'this' keyword

--- extras/thisKeyword.js
+++ extras/thisKeyword.js
@@ -37,3 +37,23 @@ window binding
 //
 // me.sayName();
 // you.sayName();
+
+var Person = function(name, age) {
+  return {
+    name: name,
+    age: age,
+    sayName: function() {
+      console.log(this.name)
+    },
+    mother: {
+      name: 'Stacy',
+      sayName: function() {
+        console.log(this.name)
+      }
+    }
+  }
+}
+
+var jim = Person('Jim', 42)
+jim.sayName()
+jim.mother.sayName()
