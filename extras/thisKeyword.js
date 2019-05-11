82c025b004e65c0fdd41a44a4fcf9778101e6e2a
shivprogrammer <shiv.unadkat@gmail.com>
2019-05-11 02:43:28 -0700

    this keyword within explicit binding contexts

--- extras/thisKeyword.js
+++ extras/thisKeyword.js
@@ -38,22 +38,35 @@ window binding
 // me.sayName();
 // you.sayName();
 
-var Person = function(name, age) {
-  return {
-    name: name,
-    age: age,
-    sayName: function() {
-      console.log(this.name)
-    },
-    mother: {
-      name: 'Stacy',
-      sayName: function() {
-        console.log(this.name)
-      }
-    }
-  }
+// var Person = function(name, age) {
+//   return {
+//     name: name,
+//     age: age,
+//     sayName: function() {
+//       console.log(this.name)
+//     },
+//     mother: {
+//       name: 'Stacy',
+//       sayName: function() {
+//         console.log(this.name)
+//       }
+//     }
+//   }
+// }
+
+// var jim = Person('Jim', 42)
+// jim.sayName()
+// jim.mother.sayName()
+
+// EXPLICIT BINDING //
+var sayName = function() {
+  console.log('My name is ' + this.name)
+}
+var stacy = {
+  name: 'Stacy',
+  age: 40
 }
 
-var jim = Person('Jim', 42)
-jim.sayName()
-jim.mother.sayName()
+var languages = ['JavaScript', 'Ruby', 'Python']
+
+sayName.call(stacy)
